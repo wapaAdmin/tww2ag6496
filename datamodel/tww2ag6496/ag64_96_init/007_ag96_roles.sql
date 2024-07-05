5fad88a99b475587f880ac4662d97b7ad8959ecf
cymed <102035587+cymed@users.noreply.github.com>
2024-07-05 15:30:32 +0200

    Roles management

--- datamodel/tww2ag6496/ag64_96_init/007_ag96_roles.sql
+++ datamodel/tww2ag6496/ag64_96_init/007_ag96_roles.sql
@@ -4,24 +4,24 @@
 
 /* User */
 
-
 DO
 $$
+DECLARE
+	viewer_role text;
+	user_role text;	
 BEGIN
-  IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname='tww_user_{db_identifier}') THEN
-	GRANT ALL ON SCHEMA tww_ag6496 TO tww_user_{db_identifier};
-	GRANT ALL ON ALL TABLES IN SCHEMA tww_ag6496 TO tww_user_{db_identifier};
-	GRANT ALL ON ALL SEQUENCES IN SCHEMA tww_ag6496 TO tww_user_{db_identifier};
-	GRANT ALL ON ALL TABLES IN SCHEMA tww_cfg TO tww_user_{db_identifier};
-	ALTER DEFAULT PRIVILEGES IN SCHEMA tww_ag6496 GRANT ALL ON TABLES TO tww_user_{db_identifier};
-	ALTER DEFAULT PRIVILEGES IN SCHEMA tww_ag6496 GRANT ALL ON SEQUENCES TO tww_user_{db_identifier};
+ SELECT  rolname FROM pg_roles WHERE rolname='tww_user_'||regexp_replace(current_catalog, 'tww_|teksi_', '') INTO user_role;
+  IF FOUND THEN
+	BEGIN
+		EXECUTE FORMAT('GRANT ALL ON SCHEMA tww_ag6496 TO %I', user_role);
+		EXECUTE FORMAT('GRANT ALL ON ALL TABLES IN SCHEMA tww_ag6496 TO %I', user_role);
+		EXECUTE FORMAT('ALTER DEFAULT PRIVILEGES IN SCHEMA tww_ag6496 GRANT ALL ON TABLES TO %s TO %I', user_role);
+    END
   ELSE
 	GRANT ALL ON SCHEMA tww_ag6496 TO tww_user;
 	GRANT ALL ON ALL TABLES IN SCHEMA tww_ag6496 TO tww_user;
-	GRANT ALL ON ALL SEQUENCES IN SCHEMA tww_ag6496 TO tww_user;
-	GRANT ALL ON ALL TABLES IN SCHEMA tww_cfg TO tww_user;
 	ALTER DEFAULT PRIVILEGES IN SCHEMA tww_ag6496 GRANT ALL ON TABLES TO tww_user;
-	ALTER DEFAULT PRIVILEGES IN SCHEMA tww_ag6496 GRANT ALL ON SEQUENCES TO tww_user;
-  END IF;
+  END IF; 
+
 END;
 $$
